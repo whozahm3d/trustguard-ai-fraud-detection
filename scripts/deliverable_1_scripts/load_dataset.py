f4b337f9a880da3a5a10b7f839d3eb9600ec5451
Ali Ahmad <whozahm3d@gmail.com>
2026-03-20 15:41:46 +0500

    Update load_dataset.py

--- scripts/deliverable_1_scripts/load_dataset.py
+++ scripts/deliverable_1_scripts/load_dataset.py
@@ -1,1 +1,20 @@
+def load_dataset(path):
+    """
+    Load dataset from CSV file
+    """
 
+    df = pd.read_csv(path)
+
+    print("Dataset Loaded Successfully")
+    print("Shape:", df.shape)
+
+    return df
+
+
+if __name__ == "__main__":
+
+    dataset_path = "/content/PaySim - Synthetic Financial Dataset for Fraud Detection.csv"
+
+    df = load_dataset(dataset_path)
+
+    print(df.head())
