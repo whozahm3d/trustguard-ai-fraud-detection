7e4664130708b26354fd2975d4e0c8d89b49c31a
Ali Ahmad <whozahm3d@gmail.com>
2026-03-20 15:51:57 +0500

    Update feature_engineering.py

--- scripts/deliverable_1_scripts/feature_engineering.py
+++ scripts/deliverable_1_scripts/feature_engineering.py
@@ -1,1 +1,13 @@
+def encode_transaction_type(df):
+    """
+    Convert transaction type into numeric values
+    """
+
+    encoder = LabelEncoder()
+
+    df["type"] = encoder.fit_transform(df["type"])
+
+    return df
+encode_transaction_type(df)
+
 
